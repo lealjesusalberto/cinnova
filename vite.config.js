bc2e03eb84a9bd1923aeba5cf463eba0bfaefbc2
unknown <lealjesusalberto@gmail.com>
2026-09-02 13:11:25 -0400

    fix(build): add multiple html inputs for vite to fix production links

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+// If react is not actually used, we can leave the plugin as is, but we need to add the build config.
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        tyf: resolve(__dirname, 'abc-tyf.html'),
+        eva: resolve(__dirname, 'abc-eva.html'),
+      }
+    }
+  }
 })
